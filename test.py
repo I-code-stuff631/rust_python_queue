c8c40b36998deba2d628f0caf26d71e64103a7c2
Adrian Metallinos <39782071+I-code-stuff631@users.noreply.github.com>
2023-03-19 22:39:57 -0400

    Modified test_other and added more ez test trees

--- test.py
+++ test.py
@@ -4,32 +4,7 @@ import unittest  # Hacker rank
 
 class Test(unittest.TestCase):
     def test_iter(self):
-        # Test tree 2
-        self.queue.push(10)
-        self.queue.push(6)
-        self.queue.push(14)
-        self.queue.push(4)
-        self.queue.push(8)
-        self.queue.push(12)
-        self.queue.push(16)
-        self.queue.push(3)
-        self.queue.push(5)
-        self.queue.push(7)
-        self.queue.push(9)
-        self.queue.push(11)
-        self.queue.push(13)
-        self.queue.push(15)
-        self.queue.push(17)
-        ###################
-        self.queue.push(1)
-        self.queue.push(2)
-        self.queue.push(18)
-        self.queue.push(17)
-        self.queue.push(19)
-        self.queue.push(20)
-        self.queue.push(18)
-        self.queue.push(18)
-        self.queue.push(18.5)
+        self.large_test_tree()
         # 20, 19, 18.5, 18, 18, 18, 17, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 
         # Test tree 1
@@ -80,7 +55,7 @@ class Test(unittest.TestCase):
         self.assertRaises(StopIteration, queue_iter.__next__)
 
     def test_indexing(self):
-        self.ez_test_tree()
+        self.small_test_tree()
 
         self.assertEqual(10, self.queue[0])
         self.assertEqual(6, self.queue[1])
@@ -123,17 +98,26 @@ class Test(unittest.TestCase):
 
     def test_other(self):
         """For things that don't really get tested elsewhere"""
-        self.ez_test_tree()
+        self.medium_test_tree()
 
         self.assertEqual(10, self.queue.pop())
+        self.assertEqual(10, self.queue.pop())
+        self.assertEqual(10, self.queue.peek())
+        self.assertEqual(10, self.queue.pop())
+
         self.assertEqual(6, self.queue.peek())
         self.assertEqual(6, self.queue.pop())
+        self.assertEqual(6, self.queue.pop())
 
-        self.assertEqual(5, self.queue.pop())
         # Uncomment when impled
-        # del self.queue[0]
-        # self.assertEqual(3, self.queue.pop())
-        self.assertEqual(5, self.queue.pop())  # Comment when impled
+        # for _ in range(3):
+        #     del self.queue[0]
+        # Comment when impled
+        for _ in range(3):
+            self.queue.pop()
+        self.assertEqual(5, self.queue.pop())
+        self.assertEqual(3, self.queue.pop())
+
         self.queue.clear()
         self.assertEqual(None, self.queue.pop())
         self.assertEqual(None, self.queue.peek())
@@ -148,7 +132,7 @@ class Test(unittest.TestCase):
     #     self.queue.push(3)
     #     self.assertEqual("[10, 5, 5, 3, 1]", str(self.queue))
 
-    def ez_test_tree(self):
+    def small_test_tree(self):
         """10, 6, 5, 5, 3, 2, 0"""
         self.queue.push(5)
         self.queue.push(0)
@@ -157,6 +141,49 @@ class Test(unittest.TestCase):
         self.queue.push(10)
         self.queue.push(5)
         self.queue.push(6)
+
+    def medium_test_tree(self):
+        """10, 10, 10, 6, 6, 5, 5, 5, 5, 3, 3, 2, 2, 0, 0"""
+        self.small_test_tree()
+        # Test tree 1
+        self.queue.push(5)
+        self.queue.push(0)
+        self.queue.push(10)
+        self.queue.push(10)
+        self.queue.push(5)
+        self.queue.push(3)
+        self.queue.push(6)
+        self.queue.push(2)
+        # 10, 10, 6, 5, 5, 3, 2, 0
+
+    def large_test_tree(self):
+        """20, 19, 18.5, 18, 18, 18, 17, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1"""
+        # Test tree 2
+        self.queue.push(10)
+        self.queue.push(6)
+        self.queue.push(14)
+        self.queue.push(4)
+        self.queue.push(8)
+        self.queue.push(12)
+        self.queue.push(16)
+        self.queue.push(3)
+        self.queue.push(5)
+        self.queue.push(7)
+        self.queue.push(9)
+        self.queue.push(11)
+        self.queue.push(13)
+        self.queue.push(15)
+        self.queue.push(17)
+        ###################
+        self.queue.push(1)
+        self.queue.push(2)
+        self.queue.push(18)
+        self.queue.push(17)
+        self.queue.push(19)
+        self.queue.push(20)
+        self.queue.push(18)
+        self.queue.push(18)
+        self.queue.push(18.5)
 
     def setUp(self) -> None:
         self.queue = PriorityQueue()
