9771970044b4348bacd36f26d75314fa5fcb1585
Adrian Metallinos <39782071+I-code-stuff631@users.noreply.github.com>
2023-03-19 21:27:05 -0400

    Much better iter test

--- test.py
+++ test.py
@@ -4,6 +4,34 @@ import unittest  # Hacker rank
 
 class Test(unittest.TestCase):
     def test_iter(self):
+        # Test tree 2
+        self.queue.push(10)
+        self.queue.push(6)
+        self.queue.push(14)
+        self.queue.push(4)
+        self.queue.push(8)
+        self.queue.push(12)
+        self.queue.push(16)
+        self.queue.push(3)
+        self.queue.push(5)
+        self.queue.push(7)
+        self.queue.push(9)
+        self.queue.push(11)
+        self.queue.push(13)
+        self.queue.push(15)
+        self.queue.push(17)
+        ###################
+        self.queue.push(1)
+        self.queue.push(2)
+        self.queue.push(18)
+        self.queue.push(17)
+        self.queue.push(19)
+        self.queue.push(20)
+        self.queue.push(18)
+        self.queue.push(18)
+        self.queue.push(18.5)
+        # 20, 19, 18.5, 18, 18, 18, 17, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+
         # Test tree 1
         self.queue.push(5)
         self.queue.push(0)
@@ -13,24 +41,43 @@ class Test(unittest.TestCase):
         self.queue.push(3)
         self.queue.push(6)
         self.queue.push(2)
+        # 10, 10, 6, 5, 5, 3, 2, 0
 
         queue_iter = iter(self.queue)
+        self.assertEqual(20, next(queue_iter))
+        self.assertEqual(19, next(queue_iter))
+        self.assertEqual(18.5, next(queue_iter))
+        self.assertEqual(18, next(queue_iter))
+        self.assertEqual(18, next(queue_iter))
+        self.assertEqual(18, next(queue_iter))
+        self.assertEqual(17, next(queue_iter))
+        self.assertEqual(17, next(queue_iter))
+        self.assertEqual(16, next(queue_iter))
+        self.assertEqual(15, next(queue_iter))
+        self.assertEqual(14, next(queue_iter))
+        self.assertEqual(13, next(queue_iter))
+        self.assertEqual(12, next(queue_iter))
+        self.assertEqual(11, next(queue_iter))
         self.assertEqual(10, next(queue_iter))
         self.assertEqual(10, next(queue_iter))
+        self.assertEqual(10, next(queue_iter))
+        self.assertEqual(9, next(queue_iter))
+        self.assertEqual(8, next(queue_iter))
+        self.assertEqual(7, next(queue_iter))
+        self.assertEqual(6, next(queue_iter))
         self.assertEqual(6, next(queue_iter))
         self.assertEqual(5, next(queue_iter))
         self.assertEqual(5, next(queue_iter))
+        self.assertEqual(5, next(queue_iter))
+        self.assertEqual(4, next(queue_iter))
+        self.assertEqual(3, next(queue_iter))
         self.assertEqual(3, next(queue_iter))
         self.assertEqual(2, next(queue_iter))
+        self.assertEqual(2, next(queue_iter))
+        self.assertEqual(1, next(queue_iter))
         self.assertEqual(0, next(queue_iter))
         self.assertRaises(StopIteration, queue_iter.__next__)
         self.assertRaises(StopIteration, queue_iter.__next__)
-        del queue_iter
-    #
-    #     self.queue = PriorityQueue()
-    #     # Test tree 2
-    #     self.queue.push()
-    #     self.queue.push()
 
     def test_indexing(self):
         self.queue.push(7)
@@ -92,13 +139,5 @@ class Test(unittest.TestCase):
         self.queue = PriorityQueue()
 
 
-def test():
-    queue = PriorityQueue()
-    queue.push(1)
-    queue.push(1)
-    queue.peek()
-
-
 if __name__ == '__main__':
-    test()
-    # unittest.main()
+    unittest.main()
